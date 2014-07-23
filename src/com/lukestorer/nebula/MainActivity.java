918e4699ef8aad43af4119a496d9ef43c54b3dce
Luke Storer <luke.storer@jadu.net>
2014-07-23 16:12:36 +0100

    [FEATURE] basic loading of specified URL

--- src/com/lukestorer/nebula/MainActivity.java
+++ src/com/lukestorer/nebula/MainActivity.java
@@ -2,14 +2,27 @@ package com.lukestorer.nebula;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity
 {
+	private WebView webView;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        /** Get URL from XML resources. */
+        String url = getString(R.string.app_url);
+
+        /** Populate webview conponent and launch URL */
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl(url);
     }
 }
