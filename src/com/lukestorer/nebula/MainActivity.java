3e53c67e2e102ed7115a3bc3dc210b111d0cd276
Luke Storer <luke.storer@jadu.net>
2014-07-24 17:05:53 +0100

    [FEATURE] only allow navigation to URLS that are part of the bundled site, external URL's will be launched in a new activity using the users preferred browser

--- src/com/lukestorer/nebula/MainActivity.java
+++ src/com/lukestorer/nebula/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
+import android.net.Uri;
+import android.content.Intent;
+
 
 public class MainActivity extends Activity
 {
@@ -19,9 +22,25 @@ public class MainActivity extends Activity
         /** Get URL from XML resources. */
         String url = getString(R.string.app_url);
 
+        url = "http://" + url;
+
         /** Populate webview conponent and launch URL */
         webView = (WebView) findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                String appUrl = getString(R.string.app_url);
+
+                if (Uri.parse(url).getHost().equals(appUrl)) {
+                    // This is my web site, so do not override; let my WebView load the page
+                    return false;
+                }
+                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+        });
 		webView.getSettings().setJavaScriptEnabled(true);
 		webView.loadUrl(url);
     }
